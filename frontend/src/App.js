f9138f4c4809539cca40bc80e193df6ccc754eac
PhillipHarden <phillipiscoding@gmail.com>
2025-04-10 16:08:40 -0400

    2:6

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to E-Shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App
