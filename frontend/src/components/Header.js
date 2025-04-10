505e98838328b9dd281e38a5d7f8979bc16733a4
PhillipHarden <phillipiscoding@gmail.com>
2025-04-10 16:40:13 -0400

    3:7

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,16 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import Logo from "../assets/images/logo.png";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">E-Shop</Navbar.Brand>
+          <Navbar.Brand href="/">
+          <img src={Logo} alt="shopping cart logo" />
+          
+          E-Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
